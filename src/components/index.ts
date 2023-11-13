098d3793c5839f498772b0daa4a707b283c9c87c
Wadefeng1 <714570455@qq.com>
2023-11-13 16:30:28 +0800

    fix: optimize code structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export * from "@/components/NavigateSample";
 export * from "@/components/FormikAndYupSample";
 export * from "@/components/Demo";
 export { default as Demo2 } from "@/components/Demo2";
+export { default as Carousel } from "@/components/Carousel";
